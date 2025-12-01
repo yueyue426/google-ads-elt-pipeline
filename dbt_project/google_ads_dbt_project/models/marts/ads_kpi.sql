bed15eb988db08006d5a0f8e2bcba274ceba1bee
Chloe Jiang <yueyue426@berkeley.edu>
2025-12-01 12:37:20 -0800

    Add KPI model

---
+++ dbt_project/google_ads_dbt_project/models/marts/ads_kpi.sql
@@ -0,0 +1,33 @@
+{{ config(materialized = 'table') }}
+
+WITH base AS (
+    SELECT
+        date_id,
+        campaign_id,
+        location_id,
+        device_id,
+        clicks,
+        impressions,
+        conversions,
+        cost,
+        sale_amount
+    FROM {{ ref('fact_ads_performance') }}
+)
+SELECT
+    date_id,
+    campaign_id,
+    location_id,
+    device_id,
+    clicks,
+    impressions,
+    conversions,
+    cost,
+    sale_amount,
+    -- KPI metrics
+    SAFE_DIVIDE(clicks, impressions) AS CTR, -- clicks per impression
+    SAFE_DIVIDE(cost, clicks) AS CPC, -- cost per clicks
+    SAFE_DIVIDE(cost, conversions) AS CPA, -- cost per conversion
+    SAFE_DIVIDE(conversions, clicks) AS coverstion_rate,
+    SAFE_DIVIDE(sale_amount - cost, cost) AS ROI,
+    sale_amount - cost AS profit
+FROM base;
